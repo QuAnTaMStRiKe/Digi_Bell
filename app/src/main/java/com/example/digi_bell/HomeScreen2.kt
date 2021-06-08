4082cae51b8dad8a47e5a113989cdf359bd8c839
ANSH <quantamstrike@gmail.com>
2021-06-08 09:46:52 +0530

    Commit 2

---
+++ app/src/main/java/com/example/digi_bell/HomeScreen2.kt
@@ -0,0 +1,11 @@
+package com.example.digi_bell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen2)
+    }
+}
