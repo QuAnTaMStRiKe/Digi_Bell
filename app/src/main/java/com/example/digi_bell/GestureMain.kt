e2b9a1758b6bb72b58525768c83df1d6d2ffafef
Jaguar <rishabhkumar9393@gmail.com>
2021-06-08 11:45:32 +0530

    Gesture updated

---
+++ app/src/main/java/com/example/digi_bell/GestureMain.kt
@@ -0,0 +1,11 @@
+package com.example.digi_bell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GestureMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gesture_main)
+    }
+}
