4d81cdf13d93241d9066fce5c6d54f0d2936b017
ANSH <quantamstrike@gmail.com>
2021-06-28 18:35:45 +0530

    Navigation Drawer

--- app/src/main/java/com/example/digi_bell/HomeScreen1.kt
+++ app/src/main/java/com/example/digi_bell/HomeScreen1.kt
@@ -4,8 +4,13 @@ package com.example.digi_bell
 import android.content.Intent
 import android.os.*
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -15,6 +20,11 @@ import kotlinx.android.synthetic.main.activity_home_screen1.*
 
 
 class HomeScreen1 : AppCompatActivity() {
+
+
+    private var abdt: ActionBarDrawerToggle? = null
+
+
 
     private lateinit var auth: FirebaseAuth
     private lateinit var dbRef: DatabaseReference
@@ -63,6 +73,42 @@ class HomeScreen1 : AppCompatActivity() {
             val int = Intent(this, UserProfile::class.java)
             startActivity(int)
         }
+
+
+        abdt = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        abdt!!.isDrawerIndicatorEnabled = true
+        drawer_layout!!.addDrawerListener(abdt!!)
+        abdt!!.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+        navigation!!.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.profile -> {
+                    Log.e("Account", "My Account Clicked")
+                    Toast.makeText(this, "My Account", Toast.LENGTH_SHORT).show()
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.qrCode -> {
+                    Toast.makeText(this, "Show QR Code", Toast.LENGTH_SHORT).show()
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.bcmReceiver -> {
+                    Toast.makeText(this, "Become Receiver", Toast.LENGTH_SHORT).show()
+                    return@setNavigationItemSelectedListener true
+                }
+            }
+             return@setNavigationItemSelectedListener true
+        }
+
     }
 
 
@@ -72,5 +118,10 @@ class HomeScreen1 : AppCompatActivity() {
         dbRef.child("Help").setValue(sendHelp)
         Toast.makeText(this, "Asking Help", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (abdt!!.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item!!)
+    }
+
 
 }
